0769da1c20d569014caf8c5f1c1c3e556a35e85a
ngc7331 <ngc7331@outlook.com>
2025-06-16 23:59:39 +0800

    fix: sync upstream mesonEmulatorHook

--- modules/t-head-hack/mesonEmulatorHook/overlay.nix
+++ modules/t-head-hack/mesonEmulatorHook/overlay.nix
@@ -4,18 +4,23 @@ self: super: {
   # override original `nixpkgs/pkgs/top-level/all-packages.nix`.mesonEmulatorHook
   # to add t-head vendor extensions to QEMU arguments.
   mesonEmulatorHook =
-    if (super.stdenv.buildPlatform != super.stdenv.targetPlatform) then
-      super.makeSetupHook
-        {
-          name = "mesonEmulatorHook";
-          substitutions = {
-            crossFile = super.writeText "cross-file.conf" ''
-              [binaries]
-              exe_wrapper = [${super.lib.escapeShellArg (super.stdenv.targetPlatform.emulator super.buildPackages)}, '-cpu', '${qemu-cpu}']
-            '';
-          };
-        } ./emulator-hook.sh # FIXME: how to use nixpkgs?
-    else
-      throw "mesonEmulatorHook has to be in a cross conditional i.e. (stdenv.buildPlatform != stdenv.hostPlatform)"
-    ;
+    super.makeSetupHook
+      {
+        name = "mesonEmulatorHook";
+        substitutions = {
+          crossFile = super.writeText "cross-file.conf" ''
+            [binaries]
+            exe_wrapper = ['${super.lib.escape [ "'" "\\" ] (super.stdenv.targetPlatform.emulator super.buildPackages)}', '-cpu', '${qemu-cpu}']
+          '';
+        };
+      }
+      # The throw is moved into the `makeSetupHook` derivation, so that its
+      # outer level, but not its outPath can still be evaluated if the condition
+      # doesn't hold. This ensures that splicing still can work correctly.
+      (
+        if (super.stdenv.buildPlatform != super.stdenv.targetPlatform) then
+          ./emulator-hook.sh # FIXME: how to use nixpkgs?
+        else
+          throw "mesonEmulatorHook may only be added to nativeBuildInputs when the target binaries can't be executed; however you are attempting to use it in a situation where ${super.stdenv.hostPlatform.config} can execute ${super.stdenv.targetPlatform.config}. Consider only adding mesonEmulatorHook according to a conditional based canExecute in your package expression."
+      );
 }
